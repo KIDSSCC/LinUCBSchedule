df6f8b383445ef9325815458e03c86a502560044
KIDSSCC <18147667093@163.com>
2024-07-16 05:53:15 +0000

    refactor preliminary LinUCB schedule

---
+++ LinUCB.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+class LinUCB:
+    def __init__(self, alpha, n_arms, n_features):
+        self.alpha = alpha
+        self.n_arms = n_arms
+        self.n_features = n_features
+        self.A = np.array([np.identity(n_features) for _ in range(n_arms)])
+        self.b = np.array([np.zeros(n_features) for _ in range(n_arms)])
+
+    def select_arm(self, context, factor_alpha):
+        p = np.zeros(self.n_arms)
+        self.alpha *= factor_alpha
+        
+        # print("n_arms:")
+        print(self.n_arms)
+
+        for arm in range(self.n_arms):
+            A_inv = np.linalg.inv(self.A[arm])
+            theta = np.dot(A_inv, self.b[arm])
+            p[arm] = np.dot(theta.T, context) + self.alpha * np.sqrt(np.dot(context.T, np.dot(A_inv, context)))
+        return np.argmax(p)
+
+    def update(self, chosen_arm, reward, context):
+        self.A[chosen_arm] += np.outer(context, context)
+        self.b[chosen_arm] += reward * context
+
+def gen_all_config(num_apps, num_resources):
+    '''
+    generate all resource config according to the number of apps and total resources
+
+    Args:
+        num_apps (int): number of apps
+        num_resources (int): total units of resources
+
+    Returns:
+        list<list>: a list containing all possible config, which is list<int>
+    '''
+    if num_apps == 1:
+        # Only one app, it get all remaining resources
+        return [[num_resources]]
+    
+    all_config = []
+    for i in range(num_resources + 1):
+        # Recursively allocate the remaining resources among the remaining app
+        for sub_allocation in gen_all_config(num_apps - 1, num_resources - i):
+            all_config.append([i] + sub_allocation)
+    return all_config
+
+def get_now_reward(curr_metrics):
+    '''
+    Get a default context and average reward
+
+    Args:
+        curr_metrics (list<float>): a feedback metric representing the current mixed deployment status for each app
+
+    Returns:
+        list<float>: context infomation, initialize as a list of 18 elements, each set to 1.0
+        float: th_reward, the average reward calculated based on the current metrics
+    '''
+    context = [1.0 for _ in range(18)]
+    th_reward = sum(float(x) for x in curr_metrics)/len(curr_metrics)
+    return context, th_reward
---
+++ main.py
@@ -0,0 +1,82 @@
+import csv
+import os
+import sys
+
+import numpy as np
+from LinUCB import *
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+from util import *
+
+NUM_RESOURCES_CACHE = 30  #total units of cache size 
+
+
+
+
+
+if __name__ == '__main__':
+    cm = config_management()
+    curr_config = cm.receive_config()
+    # print(old_config)
+
+    num_apps = len(curr_config[0])
+    num_resources = [NUM_RESOURCES_CACHE]
+    all_cache_config = gen_all_config(num_apps, num_resources[0])
+    num_config = len(all_cache_config)
+
+    epochs = 200
+    file_ = open('ttt.csv', 'w', newline='')
+    file_writer = csv.writer(file_)
+
+    app_name = curr_config[0]
+    cache_config = curr_config[1]
+    cache_hit = curr_config[2]
+    cache_reward = curr_config[3]
+    init_cache_index = all_cache_config.index(cache_config)
+
+    # context infomation and reward
+    context, th_reward = get_now_reward(cache_hit)
+
+    # initialize parameters
+    alpha = 0.4
+    factor_alpha = 0.994
+
+    n_features_cache = len(context)
+    context_cache = np.array(context)
+
+    num_arm = num_config
+    lin_ucb_cache = LinUCB(alpha, num_arm, n_features_cache)
+
+    lin_ucb_cache.update(init_cache_index, th_reward, context_cache)
+
+    for _ in range(epochs):
+        context, th_reward = get_now_reward(cache_hit)
+        context_cache = np.array(context)
+        # choose a arm
+        chosen_arm_cache = lin_ucb_cache.select_arm(context_cache, factor_alpha)
+        cache_index = chosen_arm_cache
+        # prepare new config
+        new_config = []
+        new_config.append(curr_config[0])
+        new_partition = all_cache_config[cache_index]
+        new_config.append(new_partition)
+
+        cm.send_config(new_config)
+
+        # waiting for result
+        curr_config = cm.receive_config()
+        app_name = curr_config[0]
+        cache_config = curr_config[1]
+        cache_hit = curr_config[2]
+        cache_reward = curr_config[3]
+
+        context, th_reward = get_now_reward(cache_hit)
+        context_cache = np.array(context)
+        lin_ucb_cache.update(chosen_arm_cache, th_reward, context_cache)
+        # write to log
+        file_writer.writerow([th_reward, cache_index])
+
+    file_.close()
+    
